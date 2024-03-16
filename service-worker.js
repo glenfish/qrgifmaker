534a008e9f97f2cf16d2e51b0bffbc9c16ad3ee4
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2024-03-16 12:25:32 +0700

    updated service worker

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,23 @@
-const CACHE_NAME = 'v2'; // Update version when changes are made to files
+const CACHE_NAME = 'v1';
 const urlsToCache = [
-    '/index.html',
-    '/gif.js',
-    '/qrcode.min.js',
-    '/jsQR.min.js',
-    '/index.min.js',
-    '/libgif.js',
-    './rubbable.js',
-    './gif.worker.js',
-    './service-worker.js'
+  '/index.html',
+  '/gif.js',
+  '/qrcode.min.js',
+  '/jsQR.min.js',
+  '/index.min.js',
+  '/libgif.js',
+  './rubbable.js',
+  './service-worker.js'
 ];
 
 // Install event - cache files
 self.addEventListener('install', event => {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
-  );
-});
-
-// Activate event - clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
 
@@ -50,13 +31,23 @@ self.addEventListener('fetch', event => {
           return response;
         }
 
-        return fetch(event.request).then(
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
           function(response) {
             // Check if we received a valid response
             if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
 
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
             var responseToCache = response.clone();
 
             caches.open(CACHE_NAME)
